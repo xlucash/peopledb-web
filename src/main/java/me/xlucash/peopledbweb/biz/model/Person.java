a4c2bb7866e43d33d58bd041c15f6df701410f69
xlucash <lukiereq@gmail.com>
2022-12-21 22:12:13 +0100

    displaying list of people, bootstrap table for people, formatting dates and salaries

---
+++ src/main/java/me/xlucash/peopledbweb/biz/model/Person.java
@@ -0,0 +1,19 @@
+package me.xlucash.peopledbweb.biz.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Data
+@AllArgsConstructor
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private BigDecimal salary;
+
+}
---
+++ src/test/java/me/xlucash/peopledbweb/PeopledbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xlucash.peopledbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopledbWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
