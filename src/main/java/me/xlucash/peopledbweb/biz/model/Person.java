8ef8e0abc720f5b43aa617198788f12419de2e8e
xlucash <lukiereq@gmail.com>
2022-12-23 21:32:54 +0100

    validating data

--- src/main/java/me/xlucash/peopledbweb/biz/model/Person.java
+++ src/main/java/me/xlucash/peopledbweb/biz/model/Person.java
@@ -3,6 +3,7 @@ package me.xlucash.peopledbweb.biz.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,10 +19,23 @@ public class Person {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotEmpty(message = "First name can not be empty")
     private String firstName;
+
+    @NotEmpty(message = "Last name can not be empty")
     private String lastName;
+
+    @Past(message = "Date of birth must be in the past")
+    @NotNull(message = "Date of birth must be specified")
     private LocalDate dob;
+
+    @Email(message = "Email must be valid")
+    @NotEmpty(message = "Email must not be empty")
     private String email;
+
+    @DecimalMin(value = "1000.00", message = "Salary must be at least 1000")
+    @NotNull(message = "Salary must be specified")
     private BigDecimal salary;
 
 }
--- src/main/java/me/xlucash/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/me/xlucash/peopledbweb/web/controller/PeopleController.java
@@ -1,8 +1,10 @@
 package me.xlucash.peopledbweb.web.controller;
 
+import jakarta.validation.Valid;
 import me.xlucash.peopledbweb.biz.model.Person;
 import me.xlucash.peopledbweb.data.PersonRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,9 +34,12 @@ public class PeopleController {
     }
 
     @PostMapping
-    public String savePerson(Person person) {
+    public String savePerson(@Valid Person person, Errors errors) {
         System.out.println(person);
-        personRepository.save(person);
-        return "redirect:people";
+        if (!errors.hasErrors()) {
+            personRepository.save(person);
+            return "redirect:people";
+        }
+        return "people";
     }
 }
